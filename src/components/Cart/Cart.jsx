f3fc643bb4208e1ff1728b9d7db3adda8f59bdcf
emisilvag <emisilvag@gmail.com>
2022-07-07 04:47:34 -0300

    orden de compra

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,3 +1,4 @@
+import { addDoc, collection, getFirestore } from "@firebase/firestore";
 import React from "react";
 import { Link } from "react-router-dom";
 import { useCartContext } from "../../Context/CartContext";
@@ -5,6 +6,29 @@ import ItemCart from "../ItemCart/ItemCart";
 
 const Cart = () => {
   const { cart, totalPrice } = useCartContext();
+
+  const order = {
+    buyer: {
+      Nombre: "Emiliano",
+      Apellido: "Silva",
+      Email: "emisilvag@gmail.com",
+      Telefono: "0987654321",
+      Direccion: "Santa Cruz, California",
+    },
+    articulos: cart.map((articulo) => ({
+      id: articulo.id,
+      articulo: articulo.article,
+      precio: articulo.precio,
+      cantidad: articulo.cantidad,
+    })),
+    total: totalPrice(),
+  };
+
+  const handleClick = () => {
+    const db = getFirestore();
+    const ordersCollection = collection(db, "orders");
+    addDoc(ordersCollection, order).then(({ id }) => console.log(id));
+  };
 
   if (cart.length === 0) {
     return (
@@ -21,6 +45,7 @@ const Cart = () => {
         <ItemCart key={articulo.id} articulo={articulo} />
       ))}
       <p>Total: {totalPrice()}</p>
+      <button onClick={handleClick}>Generar orden de compra</button>
     </>
   );
 };
